31fb73f673ca0507facffa0f4746275ae4bac324
yosefgods7-cloud <yosefgods7@gmail.com>
2026-04-25 12:36:08 +0300

    build: Use gradlew wrapper for Android build
    Replaces direct download of gradle-wrapper.jar with the `gradle wrapper` command. This ensures the correct Gradle version is used for the Android build process in the CI pipeline. Removes now redundant check for the existence/size of the wrapper jar.

--- check_url.mjs
+++
@@ -1,4 +0,0 @@
-import https from 'https';
-https.request('https://raw.githubusercontent.com/gradle/gradle/v8.7.0/gradle/wrapper/gradle-wrapper.jar', { method: 'HEAD' }, (res) => {
-  console.log(res.statusCode);
-}).end();
